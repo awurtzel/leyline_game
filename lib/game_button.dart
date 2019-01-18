ac68a0146dad35f3b4e0dba54a5343e54eed98a8
awurtzel <adam.wurtzel@gmail.com>
2019-01-18 12:34:01 -0800

    Finish flutter tutorial.

--- lib/game_button.dart
+++ lib/game_button.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
+enum ArrowDirections {
+    UP,
+    UP_RIGHT,
+    RIGHT,
+    DOWN_RIGHT,
+    DOWN,
+    DOWN_LEFT,
+    LEFT,
+    UP_LEFT,
+}
+
 class GameButton {
     final id;
     String text;
+    ArrowDirections direction;
     Color bg;
     bool enabled;
 
     GameButton({
         this.id,
         this.text = "",
+        this.direction = ArrowDirections.UP,
         this.bg = Colors.grey,
         this.enabled = true,
     });
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:leyline_game/custom_dialog.dart';
 import 'package:leyline_game/game_button.dart';
 
 class HomePage extends StatefulWidget {
@@ -49,6 +52,132 @@ class _HomePageState extends State<HomePage> {
             }
 
             button.enabled = false;
+            int winner = checkWinner();
+            if (winner == -1) {
+                if (buttonsList.every((p) => p.text != "")) {
+                    showDialog(
+                        context: context,
+                        builder: (_) =>
+                        new CustomDialog(
+                            "Game Tied",
+                            "Press the reset button to start again.",
+                            resetGame,
+                        ),
+                    );
+                } else {
+                    activePlayer == 2 ? autoPlay() : null;
+                }
+            }
+        });
+    }
+
+    void autoPlay() {
+        var emptyCells = new List();
+        var list = new List.generate(9, (i) => i + 1);
+        for (var cellID in list) {
+            if (!(player1.contains(cellID) || player2.contains(cellID))) {
+                emptyCells.add(cellID);
+            }
+        }
+
+        var rand = new Random();
+        var randIndex = rand.nextInt(emptyCells.length - 1);
+        var cellID = emptyCells[randIndex];
+        var buttonIndex = buttonsList.indexWhere((p) => p.id == cellID);
+        playGame(buttonsList[buttonIndex]);
+    }
+
+    int checkWinner() {
+        var winner = -1;
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+            winner = 1;
+        }
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+            winner = 2;
+        }
+
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+            winner = 1;
+        }
+        if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+            winner = 2;
+        }
+
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+            winner = 1;
+        }
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+            winner = 2;
+        }
+
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+            winner = 1;
+        }
+        if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+            winner = 2;
+        }
+
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+            winner = 1;
+        }
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+            winner = 2;
+        }
+
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+            winner = 1;
+        }
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+            winner = 2;
+        }
+
+        if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
+            winner = 1;
+        }
+        if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
+            winner = 2;
+        }
+
+        if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
+            winner = 1;
+        }
+        if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+            winner = 2;
+        }
+
+        if (winner != -1) {
+            if (winner == 1) {
+                showDialog(
+                    context: context,
+                    builder: (_) =>
+                    new CustomDialog(
+                        "Player 1 Won",
+                        "Press the reset button to start again.",
+                        resetGame,
+                    )
+                );
+            } else {
+                showDialog(
+                    context: context,
+                    builder: (_) =>
+                    new CustomDialog(
+                        "Player 2 Won",
+                        "Press the reset button to start again.",
+                        resetGame,
+                    )
+                );
+            }
+        }
+        return winner;
+    }
+
+    void resetGame() {
+        if (Navigator.canPop(context)) {
+            Navigator.pop(context);
+        }
+
+        setState(() {
+            buttonsList = doInit();
         });
     }
 
@@ -58,33 +187,51 @@ class _HomePageState extends State<HomePage> {
             appBar: new AppBar(
                 title: new Text("Leyline"),
             ),
-            body: new GridView.builder(
-                padding: const EdgeInsets.all(10.0),
-                gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    childAspectRatio: 1.0,
-                    crossAxisSpacing: 9.0,
-                    mainAxisSpacing: 9.0,
-                ),
-                itemCount: buttonsList.length,
-                itemBuilder: (context, i) =>
-                new SizedBox(
-                    width: 100.0,
-                    height: 100.0,
-                    child: new RaisedButton(
-                        padding: const EdgeInsets.all(8.0),
-                        onPressed: buttonsList[i].enabled ?
-                            () => playGame(buttonsList[i]) : null,
-                        child: new Text(buttonsList[i].text,
-                            style: new TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                    new Expanded(
+                        child: new GridView.builder(
+                            padding: const EdgeInsets.all(10.0),
+                            gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3,
+                                childAspectRatio: 1.0,
+                                crossAxisSpacing: 9.0,
+                                mainAxisSpacing: 9.0,
+                            ),
+                            itemCount: buttonsList.length,
+                            itemBuilder: (context, i) =>
+                            new SizedBox(
+                                width: 100.0,
+                                height: 100.0,
+                                child: new RaisedButton(
+                                    padding: const EdgeInsets.all(8.0),
+                                    onPressed: buttonsList[i].enabled ?
+                                        () => playGame(buttonsList[i]) : null,
+                                    child: new Text(buttonsList[i].text,
+                                        style: new TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20.0
+                                        ),
+                                    ),
+                                    color: buttonsList[i].bg,
+                                    disabledColor: buttonsList[i].bg,
+                                ),
                             ),
                         ),
-                        color: buttonsList[i].bg,
-                        disabledColor: buttonsList[i].bg,
                     ),
-                ),
+                    new RaisedButton(
+                        child: new Text(
+                            "Reset",
+                            style: new TextStyle(
+                                color: Colors.white, fontSize: 20.0)
+                        ),
+                        color: Colors.red,
+                        padding: const EdgeInsets.all(20.0),
+                        onPressed: resetGame,
+                    ),
+                ],
             ),
         );
     }
